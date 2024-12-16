81c0d235dc74d2b05ff69140cd14dc9867f65c89
Mai-os <mai.ozawa.01@gmail.com>
2024-12-16 10:37:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// let isDefaultColor = true;
+
+var cls = document.getElementById("button");
+cls.addEventListener("click", () => {
+    var text = document.getElementById("text")
+    text.style.color = "red";
+})
+
+console.log(cls);
