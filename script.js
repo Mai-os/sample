918ecb7ddc651cff54efde0cc68dda75018e4315
Mai-os <mai.ozawa.01@gmail.com>
2024-12-16 11:56:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// let isDefaultColor = true;
+document.addEventListener("DOMContentLoaded", function() {
+    var cls = document.getElementById("button");
+    cls.addEventListener("click", () => {
+        var text = document.getElementById("text");
+        text.style.color = "blue";  // クリック時に文字色を青に変更
+    });
 
-var cls = document.getElementById("button");
-cls.addEventListener("click", () => {
-    var text = document.getElementById("text")
-    text.style.color = "blue";
-})
-
-console.log(cls);
+    console.log(cls);  // 正しくbutton要素が取得できているか確認
+});
